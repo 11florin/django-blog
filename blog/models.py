bc52dccca11ef4fb9d518de336d185d968985388
Florin Albu <albuflorin929@yahoo.co.uk>
2026-08-07 19:28:14 +0100

    Add def __str__ to print comments by author

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     update_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ["created_on"]
 
     def __str__(self):
         return f"{self.title} | writen by {self.author}"
@@ -27,3 +27,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
