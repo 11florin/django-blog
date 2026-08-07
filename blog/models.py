981ca5bad337caaa4183641cd545371310ab18e5
Florin Albu <albuflorin929@yahoo.co.uk>
2026-08-07 19:20:24 +0100

    Add  def __str__ to print title and author

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     update_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | writen by {self.author}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
