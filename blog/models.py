b3cae63d687a82f232b17444508c12d947fb9608
Florin Albu <albuflorin929@yahoo.co.uk>
2026-08-10 11:55:05 +0100

    Add summernote

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     update_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["created_on"]
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"{self.title} | writen by {self.author}"
@@ -29,7 +29,7 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["created_on"]
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
