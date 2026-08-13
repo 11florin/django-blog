9ac1bc5a9875d255a17b7de7b73c5b5592d70c83
Florin Albu <albuflorin929@yahoo.co.uk>
2026-08-13 16:24:30 +0100

    Add about app

---
+++ about/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import About
+
+# Create your views here.
+
+def about_page(request):
+    about = About.objects.first()
+    return render(
+        request,
+        "about/about.html",
+        {"about": about}
+    )
