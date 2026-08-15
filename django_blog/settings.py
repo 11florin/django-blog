a48de79495051fef9190c16fdefe55498e6ec20c
Florin Albu <albuflorin929@yahoo.co.uk>
2026-08-15 14:57:13 +0100

    Add code for print logged in as {user}

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['florin-web-journal-97e36c85b328.herokuapp.com',
                  '127.0.0.1',
